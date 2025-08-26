5f50c11806d637a40296a9eff055cfb1db1b5d76
an9383 <an9383@gmail.com>
2025-08-26 20:38:42 +0900

    all

--- src/main/java/mes/domain/mm/CustSendDocVo.java
+++
@@ -1,9 +0,0 @@
-package mes.domain.mm;
-
-import lombok.Data;
-
-@Data
-public class CustSendDocVo {
-	private byte[] img;
-	private String state;
-}
--- src/main/java/mes/domain/mm/WelcomeVo.java
+++
@@ -1,9 +0,0 @@
-package mes.domain.mm;
-
-import lombok.Data;
-
-@Data
-public class WelcomeVo {
-	private String welcomeText;
-	private byte[] welcomeImg;
-}
